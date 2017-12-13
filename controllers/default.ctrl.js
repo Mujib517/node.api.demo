c5cb8804ca16bce531fe77678c8e5f0cc2126d4b
Mujib517 <mujib517@gmail.com>
2017-12-13 22:33:49 +0530

    Removed Logger

--- controllers/default.ctrl.js
+++ controllers/default.ctrl.js
@@ -1,11 +1,8 @@
-var logger = require('../utilities/logger');
 
 var defaultCtrl = {
 
     get: function (req, res) {
-        logger.info({ message: "Request comes" });
         res.send("Hello Express");
-        logger.error({ message: "Error" });
     },
 
     health: function (req, res) {
